c9f3c4df541d1f5c054bcfe337d15675b4a297ab
kishan2603 <kishanrathod26032007@gmail.com>
2026-07-10 19:16:38 +0530

    Time: 12 ms (53.9%), Space: 26 MB (21.75%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,13 +2,21 @@ class Solution {
 public:
     vector<int> nums;
     int n;
-    int f(int i,int target){
-        if(i>=n) return target==0;
-        return f(i+1,target+nums[i]) + f(i+1,target-nums[i]);
+    vector<vector<int>> dp;
+    int sum;
+    int target;
+    int f(int i,int curr){
+        if(i>=n) return target==curr;
+        if(dp[i][sum+curr]!=-1) return dp[i][sum+curr];
+        return dp[i][sum+curr] = f(i+1,curr+nums[i]) + f(i+1,curr-nums[i]);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         this->nums = nums;
+        this->target = target;
         n = nums.size();
-        return f(0,target);
+        sum = 0;
+        for(int a : nums) sum+=a;
+        dp.resize(n,vector<int>(2*sum+1,-1));
+        return f(0,0);
     }
 };
