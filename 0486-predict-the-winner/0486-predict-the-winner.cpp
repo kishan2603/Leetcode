b5ee6d202e35f5cacf02e3108f50382483a3fb9e
kishan2603 <kishanrathod26032007@gmail.com>
2026-08-01 11:42:04 +0530

    Time: 2 ms (43.58%), Space: 11.1 MB (6.22%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
+    vector<vector<vector<int>>> dp;
     int f(vector<int>& nums, int i, int j, bool one){
         if(i==j){
             if(one) return nums[i];
             return 0; 
         }
-        if(one) return max(f(nums,i+1,j,!one)+nums[i],f(nums,i,j-1,!one)+nums[j]);
-        else return min(f(nums,i+1,j,!one),f(nums,i,j-1,!one));
+        if(dp[i][j][one]!=-1) return dp[i][j][one];
+        if(one) return dp[i][j][one] = max(f(nums,i+1,j,!one)+nums[i],f(nums,i,j-1,!one)+nums[j]);
+        else return dp[i][j][one] = min(f(nums,i+1,j,!one),f(nums,i,j-1,!one));
     }
     bool predictTheWinner(vector<int>& nums) {
-        if(nums.size()==1) return true;
+        int n = nums.size();
+        if(n==1) return true;
+        dp.resize(n,vector<vector<int>>(n,vector<int>(2,-1)));
         int sum = accumulate(nums.begin(),nums.end(),0);
         int sum1 = f(nums,0,nums.size()-1,true);
         int sum2 = sum - sum1;
